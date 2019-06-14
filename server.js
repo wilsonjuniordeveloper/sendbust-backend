8a6452577f10506e4827ddb1d0872ea8a3aa200f
wilson junior <wilsonjuniordeveloper@bol.com.br>
2019-06-14 19:10:08 -0300

    originw

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const bodyParser = require('body-parser');
 const nodemailer = require("nodemailer");
 const csv = require('csvtojson/v1');
 const schedule = require('node-schedule');
-const cors = require('cors')
+
 
 
 let app = express();
 app.use(cors({origin: 'https://sendbust-frontend.herokuapp.com'}))
 app.use(bodyParser.urlencoded({extended: false }))
-app.use(bodyParser.json())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 
 
