2614f4c505f58dda7879798b14988eda78b2fa56
wilson junior <wilsonjuniordeveloper@bol.com.br>
2019-06-14 19:22:18 -0300

    cors

--- server.js
+++ server.js
@@ -7,15 +7,10 @@ const schedule = require('node-schedule');
 
 
 let app = express();
-app.use(cors({origin: 'https://sendbust-frontend.herokuapp.com'}))
+app.use(allowCrossDomain)
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: false }))
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+
 
 
 
