d4ede6670789b233a05b7a5ba8e7c62b587666c7
wilson junior <wilsonjuniordeveloper@bol.com.br>
2019-06-14 19:54:46 -0300

    jeelo

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const schedule = require('node-schedule');
 
 
 let app = express();
-app.use(allowCrossDomain)
-app.use(cors())
+
 app.use(bodyParser.urlencoded({extended: false }))
+app.use(bodyParser.json())
 
-
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 
 app.get('/', function (req, res) {
